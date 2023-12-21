98c6047244ed8b788747c83118039928492a284b
calcuis <113646141+calcuis@users.noreply.github.com>
2023-12-21 09:09:53 +0800

    Update copier.py

--- copier.py
+++ copier.py
@@ -7,7 +7,7 @@ os.makedirs(output_directory, exist_ok=True)
 
 # Copy the file 100 times with names from '0' to '099' / '100' to '199' => for i in range(100, 200):
 for i in range(100):
-    output_file = os.path.join(output_directory, f"{i}.png")
+    output_file = os.path.join(output_directory, f"{i}.png") # '000' to '099' => f"{i:03d}.png"
     shutil.copy(source_file, output_file)
 
 print("Copy process completed.")
